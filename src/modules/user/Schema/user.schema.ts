7753ccc5ac958391292df34420676b6b893bf090
0xEbrahim <elsayedebrahim603@gmail.com>
2025-08-16 17:34:51 +0300

    finish user schema to include additional properties and improve structure

--- src/modules/user/Schema/user.schema.ts
+++ src/modules/user/Schema/user.schema.ts
@@ -2,11 +2,69 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
+export enum Roles {
+  ADMIN = 'ADMIN',
+  MANAGER = 'MANAGER',
+  USER = 'USER',
+}
 
+export enum ActiveStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+}
+
+export enum Gender {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+}
 @Schema({ timestamps: true })
 export class User {
-  @Prop()
+  @Prop({
+    type: String,
+    required: true,
+    minLength: [3, 'Name must be at least 3 characers'],
+    maxLength: [30, 'Name must be at most 30 characers'],
+  })
   name: string;
+
+  @Prop({ type: String, required: true, unique: true })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    minLength: [3, 'Password must be at least 3 characers'],
+    maxLength: [30, 'Password must be at most 30 characers'],
+  })
+  password: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    enum: Roles,
+  })
+  role: string;
+
+  @Prop({ type: String })
+  avatar: string;
+
+  @Prop({ type: Number })
+  age: number;
+
+  @Prop({ type: String })
+  phoneNumber: string;
+
+  @Prop({ type: String })
+  address: string;
+
+  @Prop({ type: String, enum: ActiveStatus })
+  active: string;
+
+  @Prop({ type: String })
+  verificationCode: string;
+
+  @Prop({ type: String, enum: Gender })
+  gender: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './Schema/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
+
+  constructor(@InjectModel(User.name) private User : Model<User> ){}
+
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
