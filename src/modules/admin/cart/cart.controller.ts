fe71ead7a036d34b89789b1d369ad41a8cf11a28
0xEbrahim <elsayedebrahim603@gmail.com>
2025-08-19 18:41:29 +0300

    feat: implement admin cart module with CRUD operations; add controller, service, and API documentation for cart management

--- src/modules/admin/cart/cart.controller.ts
+++ src/modules/admin/cart/cart.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { AdminCartService } from './cart.service';
+import { AuthGuard } from 'src/common/guards/Auth.guard';
+import { RolesGuard } from 'src/common/guards/Role.guard';
+import { Role } from 'src/common/decorators/roles.decorator';
+import { Roles } from '../user/Schema/user.schema';
+import { ApiBearerAuth, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { QueryDto } from 'src/common/dto/query.dto';
 
-@Controller('cart')
+@Controller('admin/cart')
+@UseGuards(AuthGuard, RolesGuard)
+@Role(Roles.ADMIN)
+@ApiBearerAuth()
+@ApiTags('Cart')
 export class AdminCartController {
   constructor(private readonly cartService: AdminCartService) {}
+
+  @ApiParam({ name: 'id', type: '68a492862c0202e173deb9fc' })
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return await this.cartService.findOne(id);
+  }
+
+  @ApiQuery({ type: QueryDto })
+  @Get()
+  async findAll(@Query() q: any) {
+    return await this.cartService.findAll(q);
+  }
 }
--- src/modules/admin/cart/cart.service.ts
+++ src/modules/admin/cart/cart.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cart } from './schema/cart.schema';
+import { Model } from 'mongoose';
+import ApiFeatures from 'src/common/utils/APIFeatures';
 
 @Injectable()
-export class AdminCartService {}
+export class AdminCartService {
+  constructor(@InjectModel(Cart.name) private CartModel: Model<Cart>) {}
+
+  async findOne(id: string) {
+    const cart = await this.CartModel.findById(id);
+    if (!cart) throw new NotFoundException('Cart not found');
+    await cart.populate('cartItems.product');
+    return { data: { cart } };
+  }
+
+  async findAll(q: any) {
+    q.page = q.page ? q.page : 1;
+    q.limit = q.limit ? q.limit : 10;
+    const query = new ApiFeatures(this.CartModel.find({}), q)
+      .filter()
+      .limitFields()
+      .sort()
+      .paginate();
+    const carts = await query.exec();
+    return { data: { carts }, page: +q.page, size: carts.length };
+  }
+}
