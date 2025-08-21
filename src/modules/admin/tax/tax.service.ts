7d59137dbb98aea5bd3420cb0982881e94c20e5f
0xEbrahim <elsayedebrahim603@gmail.com>
2025-08-21 14:26:06 +0300

    feat: enhance internationalization support in tax service; integrate i18n for error handling in tax fields validation and not found scenarios

--- src/modules/admin/tax/tax.service.ts
+++ src/modules/admin/tax/tax.service.ts
@@ -7,17 +7,21 @@ import { AdminCreateTaxDto } from './dto/create-tax.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Tax } from './schema/tax.schema';
 import { Model } from 'mongoose';
+import { I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class AdminTaxService {
-  constructor(@InjectModel(Tax.name) private TaxModel: Model<Tax>) {}
+  constructor(
+    @InjectModel(Tax.name) private TaxModel: Model<Tax>,
+    private readonly i18n: I18nService,
+  ) {}
 
   async upsert(createTaxDto: AdminCreateTaxDto) {
     let tax = await this.TaxModel.findOne({});
     if (!tax) {
       if (!createTaxDto?.shippingPrice || !createTaxDto?.taxPrice) {
         throw new BadRequestException(
-          'Please fill all the required fields: [shippingPrice, taxPrice]',
+          await this.i18n.t('service.TAX_FIELDS_REQUIRED'),
         );
       }
       tax = await this.TaxModel.create(createTaxDto);
@@ -35,13 +39,27 @@ export class AdminTaxService {
 
   async getTax() {
     const tax = await this.TaxModel.findOne({});
-    if (!tax) throw new NotFoundException('Tax not found');
+    if (!tax)
+      throw new NotFoundException(
+        await this.i18n.t('service.NOT_FOUND', {
+          args: {
+            name: await this.i18n.t('common.TAX'),
+          },
+        }),
+      );
     return { data: { tax } };
   }
 
   async deleteTax() {
     const tax = await this.TaxModel.findOne({});
-    if (!tax) throw new NotFoundException('Tax not found');
+    if (!tax)
+      throw new NotFoundException(
+        await this.i18n.t('service.NOT_FOUND', {
+          args: {
+            name: await this.i18n.t('common.TAX'),
+          },
+        }),
+      );
     await this.TaxModel.deleteOne({});
   }
 }
