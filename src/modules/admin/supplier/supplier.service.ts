7cd362b395d53489fdf6c47d83d2cd98e149cb29
0xEbrahim <elsayedebrahim603@gmail.com>
2025-08-21 14:24:12 +0300

    feat: enhance internationalization support in admin supplier service; integrate i18n for error handling in supplier existence and not found scenarios

--- src/modules/admin/supplier/supplier.service.ts
+++ src/modules/admin/supplier/supplier.service.ts
@@ -9,16 +9,25 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Supplier } from './schema/supplier.schema';
 import { Model } from 'mongoose';
 import ApiFeatures from 'src/common/utils/APIFeatures';
+import { I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class AdminSupplierService {
   constructor(
     @InjectModel(Supplier.name) private SupplierModel: Model<Supplier>,
+    private readonly i18n: I18nService,
   ) {}
 
   private async _checkValidName(name: string) {
     const supplier = await this.SupplierModel.findOne({ name: name });
-    if (supplier) throw new BadRequestException('Supplier already exists');
+    if (supplier)
+      throw new BadRequestException(
+        await this.i18n.t('service.ALREADY_EXISTS', {
+          args: {
+            name: await this.i18n.t('common.SUPPLIER'),
+          },
+        }),
+      );
   }
 
   async create(createSupplierDto: AdminCreateSupplierDto) {
@@ -41,13 +50,27 @@ export class AdminSupplierService {
 
   async findOne(id: string) {
     const supplier = await this.SupplierModel.findById(id);
-    if (!supplier) throw new NotFoundException('Supplier not found');
+    if (!supplier)
+      throw new NotFoundException(
+        await this.i18n.t('service.NOT_FOUND', {
+          args: {
+            name: await this.i18n.t('common.SUPPLIER'),
+          },
+        }),
+      );
     return { data: { supplier } };
   }
 
   async update(id: string, updateSupplierDto: AdminUpdateSupplierDto) {
     let supplier = await this.SupplierModel.findById(id);
-    if (!supplier) throw new NotFoundException('Supplier not found');
+    if (!supplier)
+      throw new NotFoundException(
+        await this.i18n.t('service.NOT_FOUND', {
+          args: {
+            name: await this.i18n.t('common.SUPPLIER'),
+          },
+        }),
+      );
     if (updateSupplierDto?.name)
       await this._checkValidName(updateSupplierDto?.name);
     supplier = await this.SupplierModel.findByIdAndUpdate(
@@ -60,7 +83,14 @@ export class AdminSupplierService {
 
   async remove(id: string) {
     const supplier = await this.SupplierModel.findById(id);
-    if (!supplier) throw new NotFoundException('Supplier not found');
+    if (!supplier)
+      throw new NotFoundException(
+        await this.i18n.t('service.NOT_FOUND', {
+          args: {
+            name: await this.i18n.t('common.SUPPLIER'),
+          },
+        }),
+      );
     await this.SupplierModel.findByIdAndDelete(id);
   }
 }
