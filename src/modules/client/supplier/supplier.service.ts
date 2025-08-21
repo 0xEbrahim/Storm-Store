aa0ff55d7363a090c138ec271141fd4088ec9984
0xEbrahim <elsayedebrahim603@gmail.com>
2025-08-21 14:06:28 +0300

    feat: enhance internationalization support in supplier service; add i18n for not found error handling and include supplier translation

--- src/modules/client/supplier/supplier.service.ts
+++ src/modules/client/supplier/supplier.service.ts
@@ -3,11 +3,13 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import ApiFeatures from 'src/common/utils/APIFeatures';
 import { Supplier } from 'src/modules/admin/supplier/schema/supplier.schema';
+import { I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class ClientSupplierService {
   constructor(
     @InjectModel(Supplier.name) private SupplierModel: Model<Supplier>,
+    private readonly i18n: I18nService,
   ) {}
 
   async findAll(q: any) {
@@ -24,7 +26,14 @@ export class ClientSupplierService {
 
   async findOne(id: string) {
     const supplier = await this.SupplierModel.findById(id);
-    if (!supplier) throw new NotFoundException('Supplier not found');
+    if (!supplier)
+      throw new NotFoundException(
+        await this.i18n.t('service.NOT_FOUND', {
+          args: {
+            name: await this.i18n.t('common.SUPPLIER'),
+          },
+        }),
+      );
     return { data: { supplier } };
   }
 }
